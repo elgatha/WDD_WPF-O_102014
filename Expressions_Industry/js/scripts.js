849be8a206ef53d23ae69b7f8dbd0da9347b9b3b
elgatha <appleelgatha@fullsail.edu>
2014-11-08 00:35:31 -0500

    job

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -10,4 +10,6 @@ Expressions_Industry
 
 //"Awesomeness" inspired by Jack Black in Kung Fu Panda because my youngest niece is 3 years old and loves it and I'm stuck babysitting
 
+//name of job
+var skill = prompt('What skill did you practice to be an Amazing Chef  For example - "knife skills"');
 
