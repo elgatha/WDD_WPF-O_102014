522ff71256ff92ec988f11f37c5df7b284b04a9f
elgatha <appleelgatha@fullsail.edu>
2014-11-08 00:47:33 -0500

    var-2

--- Expressions_Wacky/js/scripts.js
+++ Expressions_Wacky/js/scripts.js
@@ -6,3 +6,4 @@ WPF 144 - Assignment: Expressions_Wacky
 //problem: give a list of your favourite cuisine, beer, and sweets, to make fabulous fare
 
 var favCuisine = prompt('List three of your favourite types of cuisine, separated by commas');
+var favBeer = prompt('List three of your favourite types of beers, separated by commas');
