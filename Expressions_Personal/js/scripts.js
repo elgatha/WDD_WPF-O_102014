2a73a741e845e9e3a2f5225e2416ccb1c6a76fed
elgatha <appleelgatha@fullsail.edu>
2014-11-07 20:13:30 -0500

    explanation

--- Expressions_Personal/js/scripts.js
+++ Expressions_Personal/js/scripts.js
@@ -3,7 +3,7 @@ Elgatha Lee
 WPF 144 - Assignment: Expressions_Personal
 */
 
-//problem: how much is it going to cost me to replace the transmission in my Mini Cooper?
+//problem: my car was purchased a few months ago, and the transmission went kaput shortly after, but now I am stuck with what replacing it so - how much is it going to cost me to replace the transmission in my Mini Cooper?
 
 //prompt for tow truck call - Roadside Assistance
 var costOfTow = parseInt(prompt('How much did it cost to tow Mini Cooper to Mechanic Shop at Oakland Park Blvd from Boca Raton?'));
