23ea7bf97354af67c2dc67f6818da9b8dc8dfe02
elgatha <appleelgatha@fullsail.edu>
2014-11-07 20:06:53 -0500

    additions

--- Expressions_Personal/js/scripts.js
+++ Expressions_Personal/js/scripts.js
@@ -3,10 +3,10 @@ Elgatha Lee
 WPF 144 - Assignment: Expressions_Personal
 */
 
-//problem: how much is it going to cost me to replace the transmission in my mini cooper?
+//problem: how much is it going to cost me to replace the transmission in my Mini Cooper?
 
 //prompt for tow truck call - Roadside Assistance
-var costOfTow = parseInt(prompt('How much did it cost to tow Mini Cooper to Mechanic Shop?'));
+var costOfTow = parseInt(prompt('How much did it cost to tow Mini Cooper to Mechanic Shop at Oakland Park Blvd from Boca Raton?'));
 //prompt for hours mechanic works on the car - multiply by two so we can drop and replace
 var mechanicTime = parseInt(prompt('How long will it take for the mechanic to drop the transmission and then replace it?'))*2;
 //prompt for cost of transmission
