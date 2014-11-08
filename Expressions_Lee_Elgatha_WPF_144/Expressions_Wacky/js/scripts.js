2580a57851ace4fba3b209dee78b767a9664afb4
elgatha <appleelgatha@fullsail.edu>
2014-11-07 20:04:13 -0500

    +

--- Expressions_Lee_Elgatha_WPF_144/Expressions_Wacky/js/scripts.js
+++ Expressions_Lee_Elgatha_WPF_144/Expressions_Wacky/js/scripts.js
@@ -9,22 +9,18 @@ var favCuisine = prompt('List three of your favourite types of cuisine, separate
 var favBeer = prompt('List three of your favourite types of beers, separated by commas');
 var favSweetTooth = prompt('List three of your favourite types of desserts for your Sweet-Tooth, separated by commas');
 
-
 //convert strings into arrays
 favCuisine = favCuisine.split(',');
 favBeer = favBeer.split(',');
 favSweetTooth = favSweetTooth.split(',');
-
 
 //check to see if the user has entered three comma separated items
 if(favCuisine.length != 3 || favBeer.length != 3 || favSweetTooth.length != 3){
 	alert('Please enter three of each of the items separated by commas');
 }
 
-
 //create variables for fabulous fare
 var fareSuggestions = new Array();
-
 
 //create three suggestions
 for(var i = 0; i < 3; i++){
@@ -39,7 +35,6 @@ for(var i = 0; i < 3; i++){
 	//concatenate and push into an array
 	fareSuggestions.push(randomCuisine.trim()+' + '+randomBeer.trim()+' + '+randomSweetTooth.trim());
 }
-
 
 //concatenate
 var catString = 'Chef Apple-Elgatha recommends the following pairings: ('+fareSuggestions[0]+'), ('+fareSuggestions[1]+'), ('+fareSuggestions[2]+')';
