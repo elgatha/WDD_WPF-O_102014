ce09dfbb1e078bde01996980a313d17e0d93e48d
elgatha <appleelgatha@fullsail.edu>
2014-11-08 00:34:32 -0500

    problem

--- Expressions_Industry/js/scripts.js
+++ Expressions_Industry/js/scripts.js
@@ -4,3 +4,9 @@ WPF 144 - Assignment:
 Expressions_Industry
 11/06/2014
 */
+
+
+//Problem: given a job and number of hours worked, how many hours until technically becoming awesome at being a Chef?
+
+
+
