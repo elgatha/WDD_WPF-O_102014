42c2134ccfd2936de20af8c52aed067b0581c397
elgatha <appleelgatha@fullsail.edu>
2014-11-13 22:12:25 -0500

    promptor

--- Conditionals_Industry/js/scripts.js
+++ Conditionals_Industry/js/scripts.js
@@ -13,3 +13,21 @@ function validateSelectPrompt(promptedText, errorNotice){
 	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
 	//run prompt, combine error notice with prompted text if available
 	var promptor = prompt(errorNotice+promptedText);
+
+
+	//if string is empty
+	if(promptor == ''){
+		//call function again with extra help
+		validateSelectPrompt(promptedText, 'Please provide the necessary information needed.  ');
+	//else no error
+	}else{
+		//return for storage
+		return promptor;
+	}
+}
+
+//validation for promptor expecting numeric value
+function validateValuePrompt(promptedText, errorNotice){
+	//if error notice is not clearly defined set to an empty string
+	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
+
