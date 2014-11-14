f8341fc8f5a76ef1ac4f6cca0112530e5bb0cf83
elgatha <appleelgatha@fullsail.edu>
2014-11-13 23:56:17 -0500

    corrections

--- Conditionals_Industry/js/scripts.js
+++ Conditionals_Industry/js/scripts.js
@@ -12,66 +12,66 @@ function validateSelectPrompt(promptedText, errorNotice){
 	//if error notice is undefined set to empty string
 	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
 	//run prompt, combine error notice with prompted text if available
-	var promptor = prompt(errorNotice+promptedText);
+	var prompter = prompt(errorNotice+promptedText);
 
 
 	//if string is empty
-	if(promptor == ''){
+	if(prompter == ''){
 		//call function again with extra help
 		validateSelectPrompt(promptedText, 'Please provide the necessary information needed.  ');
 	//else no error
 	}else{
 		//return for storage
-		return promptor;
+		return prompter;
 	}
 }
 
-//validation for promptor expecting numeric value
+//validation for prompter expecting numeric value
 function validateValuePrompt(promptedText, errorNotice){
 	//if error notice is not clearly defined set to an empty string
 	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
 
 
-//run promptor, combining error notice with prompt text
-	var promptor = parseInt(prompt(errorNotice+promptedText));
+//run prompter, combining error notice with prompt text
+	var prompter = parseInt(prompt(errorNotice+promptedText));
 	//if measurement is not received
 
 
-	if(isNaN(promptor)){
+	if(isNaN(prompter)){
 		//call the function again with additional help
 		validateValuePrompt(promptText, 'Please provide numeric input for recipe measurement.  ');
 	//else no error
 	
 	}else{
 		//return for storage
-		return promptor;
+		return prompter;
 	}
 }
 
 
-//validation for promptor expecting a yay or nay
+//validation for prompter expecting a yay or nay
 function validateBooleanPrompt(promptedText, errorNotice){
 //if error text is undefined set to empty string, otherwise use input
 	
 	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
-	//run the promptor, combine the error notice with prompt text
-	var promptor = prompt(errorNotice+promptedText);
+	//run the prompter, combine the error notice with prompt text
+	var prompter = prompt(errorNotice+promptedText);
 	//if there is an empty string
-	if(promptor == ''){
+	if(prompter == ''){
 		//call the function again
 		validateBooleanPrompt(promptedText, 'Please provide some additional information.  ');
 	//else if the user replies yay or true
-	}else if(promptor == 'yay' || promptor == 'true'){
+	}else if(prompter == 'yay' || prompter == 'true'){
 		//return for storage
 		return true;
 	//else if user replies nay or false
-	}else if(promptor == 'nay' || promptor == 'false'){
+	}else if(prompter == 'nay' || prompter == 'false'){
 		//return for storage
 		return false;
 	//else any other value
 	}else{
 		//call the function again with additional help text
-		validateBooleanPrompt(promptedText, 'Please reply "yay" or "nay", so that we can proceed with the task at hand.  ');
+		validateBooleanPrompt(promptedText, 'Please reply "yay", "yes" or "nay", "no", so that we can proceed with the task at hand.  ');
 	}
 }
 
@@ -82,22 +82,22 @@ function targetContextCalcPrompt(attribute, errorNotice){
 	//if error notice is undefined set to empty string
 	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
 		//use our measurement validation function to get an accurate value for attribute
-	var promptor = validateValuePrompt('What is the ingredient measurement for '+attribute+'?  This is necessary in order to accurately define the ingredient costs.');
+	var prompter = validateValuePrompt('What is the ingredient measurement for '+attribute+'?  This is necessary in order to accurately define the ingredient costs.');
 	//if attribute is measure
 	if(attribute == 'measure'){
 		//measurement conversion function is context = result;
 
 		//this is a case of finding the exact numbers
-		var attributeMeasurementPercentage = (promptor/parentMeasurement)*100;
+		var attributeMeasurementPercentage = (prompter/parentMeasurement)*100;
 		//return css string
-		return ' The exact number: '+promptor+'sp '+attributeMeasurementPercentage+'%;';
+		return ' The exact number: '+prompter+'sp '+attributeMeasurementPercentage+'%;';
 	//else if attribute is padded to have a viable inventory on hand
 
 	}else if(attribute == 'pad'){
 		//in the case of this padded context it is an important element
-		var attributeMeasurementPercentage = (promptor/targetExactMeasurements)*100;
+		var attributeMeasurementPercentage = (prompter/targetExactMeasurements)*100;
 		//return css string
-		return ' pad: '+promptor+'sp '+attributeMeasurementPercentage+'%;';
+		return ' pad: '+prompter+'sp '+attributeMeasurementPercentage+'%;';
 	//else there is an internal error
 	}else{
 		//alert the development team
@@ -105,7 +105,7 @@ function targetContextCalcPrompt(attribute, errorNotice){
 	}
 }
 
-//ask the user if the ingredients has margin & padding - return the necessary css string
+//ask the user if the ingredients
 function attributePrompt(){
 	//check if recipes has ingredients
 	var ingredientBoolean = validateBooleanPrompt('Does the recipe ingredient have an accurate measurement?');
@@ -118,7 +118,7 @@ function attributePrompt(){
 	//if measurement value prompt, store and return
 	measurementValue = (measurementBoolean) ? targetCalculatePrompt('measurement') : ' measurement: none';
 
-	//return concatenated css string 
+	//return concatenated
 	return ingredientValue+measurementValue;
 }
 
