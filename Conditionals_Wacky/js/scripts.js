639e43f16a117e13e26a44e8e3f74472e7069aee
elgatha <appleelgatha@fullsail.edu>
2014-11-13 23:42:02 -0500

    if statement

--- Conditionals_Wacky/js/scripts.js
+++ Conditionals_Wacky/js/scripts.js
@@ -10,3 +10,6 @@ Conditionals_Wacky
 function validatePreferencePrompt(promptText, type, errorNotice){
 	//if error text is undefined make the string enpty
 	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
+	//if defined type is 'zenPeaceAction'
+	if(type == 'zenPeaceAction'){
+		//build array for  zen actions
