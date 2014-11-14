4f27c25d32404ad76ae24f2530d2a03c19302722
elgatha <appleelgatha@fullsail.edu>
2014-11-13 22:44:39 -0500

    Validation Prompt

--- Conditionals_Personal/js/scripts.js
+++ Conditionals_Personal/js/scripts.js
@@ -4,3 +4,8 @@ WPF 144 - Assignment:
 Conditionals_Personal
 11/13/2014
 */
+
+//problem: Have I done enough blogging and promotion for my project...
+
+//prompt validation function
+function validatedPrompt(promptText, errorNotice){
