a3114eeafb285c2b67c52e265699e33adeb8f67e
elgatha <appleelgatha@fullsail.edu>
2014-11-13 22:11:58 -0500

    validation

--- Conditionals_Industry/js/scripts.js
+++ Conditionals_Industry/js/scripts.js
@@ -6,3 +6,10 @@ Conditionals_Industry
 */
 
 //problem: values of ingredients, return calculated percentages
+
+//validation for prompt expecting string
+function validateSelectPrompt(promptedText, errorNotice){
+	//if error notice is undefined set to empty string
+	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
+	//run prompt, combine error notice with prompted text if available
+	var promptor = prompt(errorNotice+promptedText);
