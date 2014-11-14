820caea3d2ebdb1601233ba59480b86916cad301
elgatha <appleelgatha@fullsail.edu>
2014-11-13 23:41:36 -0500

    zen

--- Conditionals_Wacky/js/scripts.js
+++ Conditionals_Wacky/js/scripts.js
@@ -4,3 +4,9 @@ WPF 144 - Assignment:
 Conditionals_Wacky
 11/13/2014
 */
+
+//problem: find your zenSpace
+
+function validatePreferencePrompt(promptText, type, errorNotice){
+	//if error text is undefined make the string enpty
+	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
