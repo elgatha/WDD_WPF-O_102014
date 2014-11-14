5298308abdeffbf8200e749d9b3427c04503b920
elgatha <appleelgatha@fullsail.edu>
2014-11-13 19:51:04 -0500

    beginning of Conditionals_

--- Conditionals_Industry/js/scripts.js
+++ Conditionals_Industry/js/scripts.js
@@ -4,3 +4,12 @@ WPF 144 - Assignment:
 Conditionals_Industry
 11/13/2014
 */
+
+//problem: values of ingredients, return calculated percentages in css format for fluidity
+
+//validation for prompt expecting string
+function validateSelectPrompt(promptedText, errorNotice){
+	//if error notice is undefined set to empty string
+	errorText = (typeof errorText === 'undefined') ? '' : errorText;
+	//run prompt, combine error notice with prompt text if available
+	var promptor = prompt(errorText+promptedText);
