8bdba5248a1d5e2bca20f74e0d9a386bbea86b0a
elgatha <appleelgatha@fullsail.edu>
2014-11-13 22:45:51 -0500

    prompt

--- Conditionals_Personal/js/scripts.js
+++ Conditionals_Personal/js/scripts.js
@@ -11,3 +11,5 @@ Conditionals_Personal
 function validatedPrompt(promptText, errorNotice){
 	//if the error is undefined make the empty string else use the input
 	errorNotice = (typeof errorNotice === 'undefined') ? '' : errorNotice;
+	//run the prompt, combine the error text with prompt text if present
+	var promptor = parseInt(prompt(errorNotice+promptText));
