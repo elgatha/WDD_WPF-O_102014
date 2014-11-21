14ad6a9e6ee224600fc10fa8af6b214749140cf1
elgatha <appleelgatha@fullsail.edu>
2014-11-20 23:18:02 -0500

    prompt user info

--- Functions_Industrial/js/scripts.js
+++ Functions_Industrial/js/scripts.js
@@ -8,3 +8,7 @@ WPF 144 - Assignment: Functions_Personal
 function promptForCode(promptedText, errorInfo){
 	//for no errorInfo
 	errorInfo = (typeof errorInfo == 'undefined') ? '' : errorInfo;
+		//prompt user information
+	var suppliedTypestring = prompt(errorInfo+promptedText);
+	
+	
