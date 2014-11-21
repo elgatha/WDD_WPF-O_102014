a6e3fe894f5e242a4abbd6db2c97b12c8d466bb8
elgatha <appleelgatha@fullsail.edu>
2014-11-20 23:27:30 -0500

    var (1)

--- Functions_Personal/js/scripts.js
+++ Functions_Personal/js/scripts.js
@@ -6,3 +6,14 @@ WPF 144 - Assignment: Functions_Personal
 
 //recipe line creator
 
+/*
+Elgatha Lee
+WPF 144 - Assignment: Functions_Personal
+*/
+
+
+//recipe line creator
+
+//create empty array to store recipe Recipes
+var addedRecipes = [];
+
