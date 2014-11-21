38e2468ab59483931094eb48fc29292635061fe2
elgatha <appleelgatha@fullsail.edu>
2014-11-20 23:18:52 -0500

    prompted Text

--- Functions_Industrial/js/scripts.js
+++ Functions_Industrial/js/scripts.js
@@ -10,5 +10,10 @@ function promptForCode(promptedText, errorInfo){
 	errorInfo = (typeof errorInfo == 'undefined') ? '' : errorInfo;
 		//prompt user information
 	var suppliedTypestring = prompt(errorInfo+promptedText);
-	
+	//check for null
+	if(suppliedTypestring == ''){
+		//if null prompt again with explanation
+		promptForCode(promptedText, 'Please provide correct information, this does not compute.');
+	//else not null
+
 	
